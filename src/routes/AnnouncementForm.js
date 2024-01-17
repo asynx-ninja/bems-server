cc00c22b0f193cc81a39398c9514ae0c176ad456
rgrgogu <obsequio.rus@gmail.com>
2024-01-17 12:28:24 +0800

    Adjusted Get Active Form

--- src/routes/AnnouncementForm.js
+++ src/routes/AnnouncementForm.js
@@ -5,9 +5,11 @@ const {
   GetAllEventsForm,
   CreateEventsForm,
   UpdateEventsForm,
+  GetActiveForm,
 } = require("../controllers/AnnouncementFormController");
 
 router.get("/", GetAllEventsForm);
+router.get("/check", GetActiveForm);
 router.post("/", CreateEventsForm);
 router.patch("/", UpdateEventsForm);
 
