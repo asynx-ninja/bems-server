bb84021145e7fa9490bcf2d1635b30fcb54c2e88
NyleChua <nyle.chua05@gmail.com>
2024-01-31 18:13:28 +0800

    updated staff - 31/01/2024

--- src/controllers/AnnouncementsController.js
+++ src/controllers/AnnouncementsController.js
@@ -75,16 +75,17 @@ const GetAllOpenBrgyAnnouncement = async (req, res) => {
 
 const CreateAnnouncement = async (req, res) => {
   try {
+    const {event_folder_id} = req.query;
     const { body, files } = req;
     const { title, details, date, brgy, isOpen } = JSON.parse(
       body.announcement
     );
     let fileArray = [];
     const event_id = GenerateID(title, brgy, "E");
-    const folder_id = await createRequiredFolders();
+    const folder_id = await createRequiredFolders(event_id, event_folder_id);
 
     for (let f = 0; f < files.length; f += 1) {
-      const { id, name } = await uploadFileDrive(files[f], folder_id);
+      const { id, name } = await uploadFolderFiles(files[f], folder_id);
 
       fileArray.push({
         link:
